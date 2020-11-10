45e0ef6a326ba1874666bdf33db684005a7d09c2
Louis Jolliet <louis.jolliet@orange.fr>
2020-11-10 09:07:49 +0100

    TDES-04 Fixing db config errors

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,11 @@
 const client = require('mongodb').MongoClient;
-const url = 'mongodb://mongo:27017';
-const dbName = 'projectsDatabase';
+const url = 'mongodb://localhost:27017';
+const dbName = 'CDP';
 const COLLECTION_NAME = "projects"
+
+module.exports = {
+  client,
+  url,
+  dbName,
+  COLLECTION_NAME
+}
