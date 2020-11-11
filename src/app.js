6d73dd216a13ac6e25813abba985b5def1b6cd61
Louis Jolliet <louis.jolliet@orange.fr>
2020-11-11 12:06:36 +0100

    TDES-04 Add auto db config

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
-const dbConfig = require('./config/db');
+const dbConfig = require('../config/db');
 
-const service = require("./services/projectService");
 const express = require('express');
 const app = express();
 const path = require("path");
 const ejs = require('ejs');
-let bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
-
 dbConfig.connectToDB();
 
 
 app.get('/', (req, res) => {
     res.send('Root');
-}).listen(8080,
-    () => console.log("App listening on port 8080"));
+});
 
+const projects = require('./routes/projects');
+
+app.use('/projects', projects);
+
+app.listen(8080, () => console.log("App listening on port 8080"));
+
+module.exports = app; // for testing
