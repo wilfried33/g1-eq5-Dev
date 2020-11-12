d02f1828a4f0b4653347ff92f2640afb737bb9e6
Louis Jolliet <louis.jolliet@orange.fr>
2020-11-12 12:47:14 +0100

    TTES-05 PUT /projects route unit test

--- test/unit/routes/project.test.js
+++ test/unit/routes/project.test.js
@@ -1,6 +1,7 @@
 process.env.NODE_ENV = 'test';
 
 const Project = require('../../../src/models/project');
+const Backlog = require('../../../src/models/backlog');
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
@@ -21,7 +22,7 @@ describe('Projects', () => {
         it('should POST a project',  () => {
             chai.request(server)
                 .post('/projects')
-                .send("key=TES3&name=projects")
+                .send("key=TES3&name=project")
                 .end((err, res) => {
                     res.should.have.status(201);
                     res.body.should.be.a('object');
@@ -30,13 +31,50 @@ describe('Projects', () => {
         it('should not POST an existing project',  () => {
             chai.request(server)
                 .post('/projects')
-                .send("key=TES3&name=projects")
+                .send("key=TES3&name=project")
                 .end((err, res) => {
                     res.should.have.status(201);
                 });
             chai.request(server)
                 .post('/projects')
-                .send("key=TES3&name=projects")
+                .send("key=TES3&name=project")
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a('object');
+                });
+        });
+
+    });
+
+    describe('TTES-05 /PUT projects', () => {
+        const name = "chaitest";
+        const key = "CTES";
+        let id;
+
+        beforeEach((done) => {
+            let project = new Project({name: name, key: key, backlog: new Backlog(), tasks: []});
+            project.save().then(() => {
+                Project.findOne({name: name}).then((p) => {
+                    id = p._id;
+                    done();
+                });
+            });
+        });
+
+        it('should PUT a project',  () => {
+            console.log(id);
+            chai.request(server)
+                .put('/projects')
+                .send("id=" + id + "&key=TES5&name=project")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                });
+        });
+        it('should not PUT a project with a wrong id',  () => {
+            chai.request(server)
+                .put('/projects')
+                .send("id=7656&key=TES5&name=projects")
                 .end((err, res) => {
                     res.should.have.status(400);
                     res.body.should.be.a('object');
