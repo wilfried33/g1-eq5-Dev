6cf9b881beb85c4f1011d95a476fc4d43b6e5bea
BuildTools <unconfigured@null.spigotmc.org>
2020-11-13 16:40:31 +0100

    TDEV-12

--- src/routes/backlog.js
+++ src/routes/backlog.js
@@ -2,3 +2,14 @@
 res.render('backlog', {projectId: id du projet, projectName: nom du projet, backlog: le backlog du project sélecioner avec toutes les us et les sprints})
 res.render('addUserStory', {projectId: id du projet, projectName: nom du projet});
 */
+const express = require('express');
+const router = express.Router();
+const projectService = require('../services/projectService');
+const backlogService = require('../services/backlogService');
+
+router.get('/create', (req, res) => {
+    const id = req.query.projectId;
+    res.render('addUserStory', {projectId:id, projectName:"project B"});
+});
+
+module.exports = router;
