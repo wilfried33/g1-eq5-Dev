2fe4317bd68140b817f61cbf55468c8d6ecc26b9
Wilfried <unconfigured@null.spigotmc.org>
2020-11-26 03:16:03 +0100

    CLOSE TDEV-42 add route GET /task/create

--- src/routes/task.js
+++ src/routes/task.js
@@ -9,6 +9,20 @@ router.get('/', (req, res) => {
     renderTask(200, req, res, id, null);
 });
 
+router.get('/create', (req, res) => {
+    const projectId = req.query.projectId;
+
+    projectService.getProject(projectId)
+        .then(project => {
+            backlogService.getUserStories(project.backlog.userStories).then(userStories => {
+                taskService.getTasks(project).then(tasks => {
+                    res.status(200).render('addTask', {project: project, userStories:userStories, tasks:tasks});
+                })
+            })
+        })
+        .catch(() => res.status(400).json({error:"Le projet n'a pas été trouvé"}));
+})
+
 function renderTask(status, req, res, projectId, error){
     projectService.getProject(projectId)
         .then(project => {
