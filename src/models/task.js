72bfebb8cb21aa58ed60d87a4df7ffffad9ac0ba
Wilfried <unconfigured@null.spigotmc.org>
2020-11-26 01:20:43 +0100

    CLOSE TDEV-45 add file addTask.ejs

--- src/models/task.js
+++ src/models/task.js
@@ -3,11 +3,11 @@ const taskSchema = new mongoose.Schema({
     id: {type: String, index: true, unique: true, required: true},
     name: {type: String, required: true},
     description: String,
-    userStoryID: {type: String, maxlength: 10},
+    userStoryID: {type: mongoose.Schema.Types.ObjectId, ref: 'UserStory'},
     timeEstimation: {type: Number, min: 0},
     assignee: {type: mongoose.Schema.Types.ObjectId, ref: 'Developer'},
-    status: {type: Number, min: 0, max: 2},
-    dependance: [{type: String}],
+    status: {type: Number, min: 0, max: 2, default:0},
+    dependency: [{type: String}],
     dod: {type: mongoose.Schema.Types.ObjectId, ref: 'DoD'}
 });
 module.exports = mongoose.model('Task', taskSchema);
