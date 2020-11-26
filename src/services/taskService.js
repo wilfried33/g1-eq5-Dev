a7bf0ec93c5565127a03b12c57ffecef7fe7f4b5
Wilfried <unconfigured@null.spigotmc.org>
2020-11-26 01:49:44 +0100

    CLOSE TDEV-41 add function addTask

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,6 +1,27 @@
 const Task = require('./../models/task');
 
 function addTask(project, name, description, usId, time, dependency) {
+    return new Promise((resolve, reject) => {
+        if(!project)
+            return reject(new Error('project parameter is required'));
+        if(!name)
+            return reject(new Error('name parameter is required'));
+        if(!description)
+            description = ""
+        if(!usId)
+            return reject(new Error('userStory parameter is required'));
+        if(!time)
+            time = 0
+
+        const index = "TDEV-51";
+        let task = new Task({id:index, name:name, description:description, userStoryID:usId, timeEstimation:time, dependency:dependency})
+        task.save()
+            .then(() => {
+                project.tasks.push(task);
+                resolve(project.save());
+            });
+    });
+
 }
 
 module.exports = {
