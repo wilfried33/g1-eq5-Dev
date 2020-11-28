1c5dc736fd77f48d9aa87a22222261430523a798
cartoonnerie <melodia@melix.org>
2020-11-28 13:13:00 +0100

    CLOSE TTDES-46 create test for addDod

---
+++ src/services/dodService.js
@@ -0,0 +1,10 @@
+const DoD = require('../../../src/models/dod');
+
+function addDod(project, name, ruleNames){
+    return new Promise((resolve, reject) => {
+    });
+}
+
+module.exports = {
+    addDod
+};
