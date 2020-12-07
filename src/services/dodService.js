c6b2b498d1b59dc804e21cd9305801745886ca28
Louis Jolliet <louis.jolliet@orange.fr>
2020-12-07 17:00:19 +0100

    FIX TTES-59 add missing requirement

--- src/services/dodService.js
+++ src/services/dodService.js
@@ -1,4 +1,5 @@
-const DoD = require('./../models/dod');
+const DoDTemplate = require('../models/dodTemplate');
+const DoD = require('../models/dod');
 
 function addDod(project, name, ruleNames){
     return new Promise((resolve, reject) => {
@@ -7,7 +8,7 @@ function addDod(project, name, ruleNames){
         if (!name)
             return reject(new Error('name parameter is required'));
 
-        let dod = new DoD({name:name, rules:ruleNames});
+        let dod = new DoDTemplate({name:name, rules:ruleNames});
         dod.save()
             .then(() => {
                 project.dods.push(dod);
@@ -25,7 +26,7 @@ function updateDod(_id, name, rules){
         if (!rules)
             return reject(new Error('ruleNames parameter is required'));
 
-        DoD.findById(_id).then((dod) => {
+        DoDTemplate.findById(_id).then((dod) => {
             dod.name = name;
             dod.rules = rules;
             resolve(dod.save());
@@ -33,7 +34,21 @@ function updateDod(_id, name, rules){
     });
 }
 
+function checkTaskDod(_dodId, ruleId, value = true){
+    return new Promise((resolve, reject) => {
+        if (!_dodId)
+            return reject(new Error('_id parameter is required'));
+        if (!ruleId)
+            return reject(new Error('ruleId parameter is required'));
+        DoD.findById(_dodId).then((dod) => {
+            dod.rules[ruleId] = value;
+            resolve(dod.save());
+        }).catch(() => reject(new Error('dod not found')));
+    });
+}
+
 module.exports = {
     addDod,
-    updateDod
+    updateDod,
+    checkTaskDod
 };
