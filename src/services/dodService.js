16906fe4682a0e9066bba59d41b51b42d9b99912
Louis Jolliet <louis.jolliet@orange.fr>
2020-12-07 13:28:12 +0100

    CLOSE TDEV-58 updateDod service implem

--- src/services/dodService.js
+++ src/services/dodService.js
@@ -11,11 +11,29 @@ function addDod(project, name, ruleNames){
         dod.save()
             .then(() => {
                 project.dods.push(dod);
-                resolve(project.save());
+                project.save().then(() => resolve(dod));
             });
     });
 }
 
+function updateDod(_id, name, rules){
+    return new Promise((resolve, reject) => {
+        if (!_id)
+            return reject(new Error('_id parameter is required'));
+        if (!name)
+            return reject(new Error('name parameter is required'));
+        if (!rules)
+            return reject(new Error('ruleNames parameter is required'));
+
+        DoD.findById(_id).then((dod) => {
+            dod.name = name;
+            dod.rules = rules;
+            resolve(dod.save());
+        }).catch(() => reject(new Error('dod not found')));
+    });
+}
+
 module.exports = {
-    addDod
+    addDod,
+    updateDod
 };
