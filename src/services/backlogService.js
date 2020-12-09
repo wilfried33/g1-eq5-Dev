abb2747bac946c6270f41bcb927ddc3cd02a5eba
Louis Jolliet <louis.jolliet@orange.fr>
2020-12-09 15:31:54 +0100

    TDEV-55 getUSByDifficulty service implem

--- src/services/backlogService.js
+++ src/services/backlogService.js
@@ -1,3 +1,5 @@
+
+
 const UserStory = require('./../models/userStory');
 const Sprint = require('./../models/sprint');
 
@@ -24,7 +26,7 @@ function getBacklog(project){
     return new Promise((resolve, reject) => {
         if (!project)
             return reject(new Error('project parameter is required'));
-        getSpints(project.backlog.sprints)
+        getSprints(project.backlog.sprints)
             .then(sprints => {
                 getUserStories(project.backlog.userStories)
                     .then(userStories =>
@@ -32,6 +34,28 @@ function getBacklog(project){
                             sprints: sprints,
                             userStories: userStories
                         }));
+            });
+    });
+}
+
+function getUSByDifficulty(project) {
+    return new Promise((resolve, reject) => {
+        if (!project)
+            return reject(new Error('project parameter is required'));
+        const usByDifficulty = [];
+        getUserStories(project.backlog.userStories)
+            .then(userStories => {
+                for (const us of userStories) {
+                    const key = us.difficulty;
+                    if (usByDifficulty.find(list => list.difficulty === key))
+                        usByDifficulty
+                            .find( list => list.difficulty === key)
+                            .userStories.push(us);
+                    else
+                        usByDifficulty.push({ difficulty: key, userStories: [us] });
+                }
+                usByDifficulty.sort((a, b) => a.difficulty - b.difficulty );
+                resolve(usByDifficulty);
             });
     });
 }
@@ -78,7 +102,7 @@ function deleteUserStory(id, project){
     });
 }
 
-function getSpints(arrayId){
+function getSprints(arrayId){
     return Sprint.find({_id:arrayId});
 }
 
@@ -179,6 +203,7 @@ module.exports = {
     updateUserStory,
     deleteUserStory,
     getUserStories,
+    getUSByDifficulty,
     getUserStory,
     getBacklog,
     addSprint,
