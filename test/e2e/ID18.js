7f58d711ab53a295da236f0c6e3407c004adefb9
Louis Jolliet <louis.jolliet@orange.fr>
2020-12-10 20:50:43 +0100

    PARTIAL TTES-62 ID18 e2e test implem

--- test/e2e/ID18.js
+++ test/e2e/ID18.js
@@ -11,7 +11,6 @@ const db = require('../../config/db');
 describe('ID18 E2E test', () => {
     let driver;
     const username = 'userlogin';
-    let url;
 
     before(async () => {
         driver = await new Builder().forBrowser('chrome').build();
@@ -23,8 +22,7 @@ describe('ID18 E2E test', () => {
         const project = await projectService.addProject('Purple Project', 'PUR');
         await driver.get('http://localhost:8080/');
         await driver.manage().addCookie({name:'project', value: project._id.toString()});
-        url = 'http://localhost:8080/task';
-        await driver.get(url);
+        await driver.get('http://localhost:8080/task');
         await driver.findElement(webdriver.By.css('#addDeveloperButton')).click();
     });
 
@@ -33,18 +31,20 @@ describe('ID18 E2E test', () => {
     });
 
     it('create a developer', async () => {
-        await driver.findElement(webdriver.By.css('#name')).sendKeys(username);
+        await driver.findElement(webdriver.By.css('#username')).sendKeys(username);
         await driver.findElement(webdriver.By.css('#validForm')).click();
-        await checkUrl(url);
+        await driver.sleep(1000);
+        await checkUrl('http://localhost:8080/task');
     });
 
     it('cancel a sprint\'s creation', async () => {
         await driver.findElement(webdriver.By.css('#rejectForm')).click();
-        await checkUrl('http://localhost:8080/developer/create');
+        await checkUrl('http://localhost:8080/task');
     });
 
     it('create a sprint with missing parameters', async () => {
         await driver.findElement(webdriver.By.css('#validForm')).click();
+        await driver.sleep(100);
         await checkUrl('http://localhost:8080/developer/create');
         let errorMessage = await driver.findElement(webdriver.By.css('#message > div')).getText();
         assert.strictEqual(errorMessage, 'Paramètre manquant ou incompatible');
